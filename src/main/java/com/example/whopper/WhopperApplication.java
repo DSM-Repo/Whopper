87086bfbc0161a619d62e294a285f9536e6dc060
dkflfkd53 <b57062562@gmail.com>
2024-05-07 22:51:30 +0900

    first commit

---
+++ src/main/java/com/example/whopper/WhopperApplication.java
@@ -0,0 +1,13 @@
+package com.example.whopper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhopperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhopperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/whopper/WhopperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whopper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhopperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
