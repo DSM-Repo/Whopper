9e2367368e30d399100451e73387ff27d21879c1
dkflfkd53 <b57062562@gmail.com>
2024-08-05 23:03:16 +0900

    add :: image uploade api 추가

---
+++ src/main/java/com/example/whopper/domain/file/api/FileController.java
@@ -0,0 +1,35 @@
+package com.example.whopper.domain.file.api;
+
+import com.example.whopper.domain.file.dto.response.ImagePathResponse;
+import com.example.whopper.domain.file.type.ImageType;
+import com.example.whopper.infra.s3.component.SaveImageComponent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/file")
+public class FileController {
+
+    private final SaveImageComponent saveImageComponent;
+
+    @PostMapping(value = "/image", consumes = {
+            MediaType.MULTIPART_FORM_DATA_VALUE
+    })
+    public Mono<ImagePathResponse> uploadImage(
+            @RequestPart("file") MultipartFile filePart,
+            @RequestParam("type") ImageType imageType) {
+
+        // Save the image using the saveImageComponent
+        return saveImageComponent.saveImage(filePart, imageType)
+                .map(path -> new ImagePathResponse(path, saveImageComponent.getFileBaseUrl()));
+    }
+}
