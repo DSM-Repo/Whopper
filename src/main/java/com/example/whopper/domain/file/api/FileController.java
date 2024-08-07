d8f9cee5cb35da4b67178d73831d4308549fc135
dkflfkd53 <b57062562@gmail.com>
2024-08-07 18:35:45 +0900

    refactor :: Reactor 관련 코드 제거

--- src/main/java/com/example/whopper/domain/file/api/FileController.java
+++ src/main/java/com/example/whopper/domain/file/api/FileController.java
@@ -1,8 +1,8 @@
 package com.example.whopper.domain.file.api;
 
+import com.example.whopper.domain.file.application.usecase.SaveImageUseCase;
 import com.example.whopper.domain.file.dto.response.ImagePathResponse;
 import com.example.whopper.domain.file.type.ImageType;
-import com.example.whopper.infra.s3.component.SaveImageComponent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,24 +11,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import reactor.core.publisher.Mono;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/file")
 public class FileController {
 
-    private final SaveImageComponent saveImageComponent;
+    private final SaveImageUseCase saveImageUseCase;
 
     @PostMapping(value = "/image", consumes = {
             MediaType.MULTIPART_FORM_DATA_VALUE
     })
-    public Mono<ImagePathResponse> uploadImage(
+    public ImagePathResponse uploadImage(
             @RequestPart("file") MultipartFile filePart,
             @RequestParam("type") ImageType imageType) {
 
         // Save the image using the saveImageComponent
-        return saveImageComponent.saveImage(filePart, imageType)
-                .map(path -> new ImagePathResponse(path, saveImageComponent.getFileBaseUrl()));
+        String path = saveImageUseCase.saveImage(filePart, imageType);
+        return new ImagePathResponse(path, saveImageUseCase.getFileBaseUrl());
     }
 }
