381837d6349e3ec24706c21b4d2325fbccc77a2c
101 <tmddnjs001133@dsm.hs.kr>
2024-08-08 22:11:01 +0900

    fix :: major

--- src/main/java/com/example/whopper/domain/major/domain/DefaultMajorFacade.java
+++ src/main/java/com/example/whopper/domain/major/domain/DefaultMajorFacade.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class DefaultMajorFacade {
-    private DefaultMajorProperties defaultMajorProperties;
+    private final DefaultMajorProperties defaultMajorProperties;
 
     public MajorEntity getDefaultMajor() {
         return new MajorEntity(defaultMajorProperties.majorId(), defaultMajorProperties.name());
