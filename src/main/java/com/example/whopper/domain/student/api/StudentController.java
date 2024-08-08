4ed6727635abd210bdb210c54be198322b41fca9
101 <tmddnjs001133@dsm.hs.kr>
2024-08-08 14:49:02 +0900

    update :: API 접근 권한 설정

--- src/main/java/com/example/whopper/domain/student/api/StudentController.java
+++ src/main/java/com/example/whopper/domain/student/api/StudentController.java
@@ -2,6 +2,7 @@ package com.example.whopper.domain.student.api;
 
 import com.example.whopper.domain.student.application.usecase.GetCurrentUserInfoUseCase;
 import com.example.whopper.domain.student.dto.GetCurrentUserInfoResponse;
+import com.example.whopper.global.annotation.OnlyStudent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class StudentController {
     private final GetCurrentUserInfoUseCase getCurrentUserInfoUseCase;
-    
+
+    @OnlyStudent
     @GetMapping("/current/info")
     public GetCurrentUserInfoResponse getCurrentUserInfoResponse() {
         return getCurrentUserInfoUseCase.get();
