465c3a352bdd659ba0c497ebabe2471e7fde4865
dkflfkd53 <b57062562@gmail.com>
2024-08-11 15:54:42 +0900

    add :: findLibraryIndex api 컨트롤러 추가

--- src/main/java/com/example/whopper/domain/library/api/LibraryController.java
+++ src/main/java/com/example/whopper/domain/library/api/LibraryController.java
@@ -1,13 +1,16 @@
 package com.example.whopper.domain.library.api;
 
+import com.example.whopper.domain.library.application.usecase.FindLibraryIndexUseCase;
 import com.example.whopper.domain.library.application.usecase.StudentFindLibraryUseCase;
 import com.example.whopper.domain.library.application.usecase.TeacherFindLibraryUseCase;
+import com.example.whopper.domain.library.dto.response.LibraryIndexResponse;
 import com.example.whopper.domain.library.dto.response.LibraryResponse;
 import com.example.whopper.global.annotation.OnlyStudent;
 import com.example.whopper.global.annotation.OnlyTeacher;
 import com.example.whopper.global.utils.DataResponseInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +24,7 @@ public class LibraryController {
 
     private final StudentFindLibraryUseCase studentFindLibraryUseCase;
     private final TeacherFindLibraryUseCase teacherFindLibraryUseCase;
+    private final FindLibraryIndexUseCase findLibraryIndexUseCase;
 
     @OnlyStudent
     @GetMapping("/student")
@@ -32,5 +36,10 @@ public class LibraryController {
     @GetMapping("/teacher")
     public DataResponseInfo<LibraryResponse> teacherFindLibrary(@RequestParam Integer year) {
         return teacherFindLibraryUseCase.teacherFindLibrary(year);
+    }
+
+    @GetMapping("/{libraryId}/index")
+    public LibraryIndexResponse findLibrary(@PathVariable String libraryId) {
+        return findLibraryIndexUseCase.findLibraryIndex(libraryId);
     }
 }
--- src/main/java/com/example/whopper/domain/library/application/impl/FindLibraryIndexService.java
+++ src/main/java/com/example/whopper/domain/library/application/impl/FindLibraryIndexService.java
@@ -1,6 +1,7 @@
 package com.example.whopper.domain.library.application.impl;
 
 import com.example.whopper.domain.library.application.component.FindLibraryByIdComponent;
+import com.example.whopper.domain.library.application.usecase.FindLibraryIndexUseCase;
 import com.example.whopper.domain.library.domain.LibraryEntity;
 import com.example.whopper.domain.library.dto.response.LibraryIndexResponse;
 import lombok.RequiredArgsConstructor;
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class FindLibraryIndexService {
+public class FindLibraryIndexService implements FindLibraryIndexUseCase {
 
     private final FindLibraryByIdComponent findLibraryByIdComponent;
 
