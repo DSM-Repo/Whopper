bfcecab965ec357e346b17e59298e51febdf958f
101 <tmddnjs001133@dsm.hs.kr>
2024-08-12 13:53:45 +0900

    update :: feedback response dto에 id 필드 추가

--- src/main/java/com/example/whopper/domain/feedback/dto/FeedbackResponse.java
+++ src/main/java/com/example/whopper/domain/feedback/dto/FeedbackResponse.java
@@ -4,11 +4,12 @@ import com.example.whopper.domain.document.domain.DocumentEntity;
 import com.example.whopper.domain.feedback.domain.FeedbackEntity;
 
 public record FeedbackResponse(
+        String id,
         String element_id,
         String element_name,
         String comment
 ) {
     public FeedbackResponse(FeedbackEntity feedback, String elementName) {
-        this(feedback.getElementId(), elementName, feedback.getComment());
+        this(feedback.getId(), feedback.getElementId(), elementName, feedback.getComment());
     }
 }
