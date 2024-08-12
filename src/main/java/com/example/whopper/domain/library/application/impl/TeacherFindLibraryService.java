caf8c174b230a83a9d4d9bb0886c3f3e864cfc2d
dkflfkd53 <b57062562@gmail.com>
2024-08-12 22:04:56 +0900

    update :: 도서관 문서 조회 api 수정

--- src/main/java/com/example/whopper/domain/library/application/impl/TeacherFindLibraryService.java
+++ src/main/java/com/example/whopper/domain/library/application/impl/TeacherFindLibraryService.java
@@ -3,6 +3,7 @@ package com.example.whopper.domain.library.application.impl;
 import com.example.whopper.domain.file.application.usecase.PdfUseCase;
 import com.example.whopper.domain.library.application.usecase.TeacherFindLibraryUseCase;
 import com.example.whopper.domain.library.dao.LibraryMongoRepository;
+import com.example.whopper.domain.library.domain.type.AccessRight;
 import com.example.whopper.domain.library.dto.LibraryResponse;
 import com.example.whopper.global.utils.DataResponseInfo;
 import lombok.RequiredArgsConstructor;
@@ -16,8 +17,11 @@ public class TeacherFindLibraryService implements TeacherFindLibraryUseCase {
     private final PdfUseCase pdfUseCase;
 
     public DataResponseInfo<LibraryResponse> teacherFindLibrary(Integer year) {
-        return DataResponseInfo.of(libraryMongoRepository.findFirstByYear(year)
-                .stream()
+        var libraries = year == null
+                ? libraryMongoRepository.findFirstByAccessRightNot(AccessRight.PRIVATE)
+                : libraryMongoRepository.findFirstByAccessRightNotAndYear(AccessRight.PRIVATE, year);
+
+        return DataResponseInfo.of(libraries.stream()
                 .map(library -> new LibraryResponse(
                         library.getId(),
                         library.getAccessRight(),
