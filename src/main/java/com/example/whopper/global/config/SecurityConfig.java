488ab8cdcd057bc99de0f1eb81ec3de114c847a9
101 <tmddnjs001133@dsm.hs.kr>
2024-08-13 10:29:56 +0900

    build:

--- src/main/java/com/example/whopper/global/config/SecurityConfig.java
+++ src/main/java/com/example/whopper/global/config/SecurityConfig.java
@@ -21,7 +21,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Collections;
 import java.util.List;
 
 @Configuration
