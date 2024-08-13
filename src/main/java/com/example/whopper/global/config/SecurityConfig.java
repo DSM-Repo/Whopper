81a40ccadbf0ec477d9fd28ada5c86489b56e34b
101 <tmddnjs001133@dsm.hs.kr>
2024-08-13 10:38:02 +0900

    build:

--- src/main/java/com/example/whopper/global/config/SecurityConfig.java
+++ src/main/java/com/example/whopper/global/config/SecurityConfig.java
@@ -88,6 +88,7 @@ public class SecurityConfig {
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
+        
         return source;
     }
 }
