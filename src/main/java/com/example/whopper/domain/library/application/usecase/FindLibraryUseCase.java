23ea265ae681b7caf6167aa6164b94a648d6fe67
101 <tmddnjs001133@dsm.hs.kr>
2024-08-20 18:57:59 +0900

    hotfix: package 경로 관련 에러 수정

--- src/main/java/com/example/whopper/domain/library/application/usecase/FindLibraryUseCase.java
+++ src/main/java/com/example/whopper/domain/library/application/usecase/FindLibraryUseCase.java
@@ -1,7 +1,7 @@
 package com.example.whopper.domain.library.application.usecase;
 
-import com.example.whopper.domain.library.dto.LibraryDetailResponse;
-import com.example.whopper.domain.library.dto.LibraryResponse;
+import com.example.whopper.domain.library.dto.response.LibraryDetailResponse;
+import com.example.whopper.domain.library.dto.response.LibraryResponse;
 import com.example.whopper.global.utils.DataResponseInfo;
 
 public interface FindLibraryUseCase {
