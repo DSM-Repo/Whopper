7c30059a78068b034b1bd42ed5409829383c1c62
101 <tmddnjs001133@dsm.hs.kr>
2024-08-20 19:05:24 +0900

    hotfix: 작동 안되는 코드 수정

--- src/main/java/com/example/whopper/domain/library/application/impl/FindLibraryIndexService.java
+++ src/main/java/com/example/whopper/domain/library/application/impl/FindLibraryIndexService.java
@@ -1,9 +1,10 @@
 package com.example.whopper.domain.library.application.impl;
 
-import com.example.whopper.domain.library.application.component.FindLibraryByIdComponent;
 import com.example.whopper.domain.library.application.usecase.FindLibraryIndexUseCase;
+import com.example.whopper.domain.library.dao.LibraryMongoRepository;
 import com.example.whopper.domain.library.domain.LibraryEntity;
 import com.example.whopper.domain.library.dto.response.LibraryIndexResponse;
+import com.example.whopper.domain.library.exception.LibraryNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +12,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class FindLibraryIndexService implements FindLibraryIndexUseCase {
 
-    private final FindLibraryByIdComponent findLibraryByIdComponent;
+    private final LibraryMongoRepository libraryMongoRepository;
 
     public LibraryIndexResponse findLibraryIndex(String libraryId) {
-        LibraryEntity library = findLibraryByIdComponent.findLibraryById(libraryId);
+        LibraryEntity library = libraryMongoRepository.findById(libraryId)
+                .orElseThrow(() -> LibraryNotFoundException.EXCEPTION);
 
         return new LibraryIndexResponse(library.getIndex());
     }
