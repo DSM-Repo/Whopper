2667c3a2883e53b745b50aee9c9544d1f2630aed
101 <tmddnjs001133@dsm.hs.kr>
2024-08-20 19:08:12 +0900

    hotfix: 작동 안되는 코드 수정

--- src/main/java/com/example/whopper/domain/library/application/impl/FindLibraryDetailService.java
+++ src/main/java/com/example/whopper/domain/library/application/impl/FindLibraryDetailService.java
@@ -1,10 +1,11 @@
 package com.example.whopper.domain.library.application.impl;
 
 import com.example.whopper.domain.file.application.usecase.PdfUseCase;
-import com.example.whopper.domain.library.application.component.FindLibraryByIdComponent;
 import com.example.whopper.domain.library.application.usecase.FindLibraryDetailUseCase;
+import com.example.whopper.domain.library.dao.LibraryMongoRepository;
 import com.example.whopper.domain.library.domain.LibraryEntity;
 import com.example.whopper.domain.library.dto.response.LibraryDetailResponse;
+import com.example.whopper.domain.library.exception.LibraryNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +13,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class FindLibraryDetailService implements FindLibraryDetailUseCase {
 
-    private final FindLibraryByIdComponent findLibraryByIdComponent;
+    private final LibraryMongoRepository libraryMongoRepository;
     private final PdfUseCase pdfUseCase;
 
     public LibraryDetailResponse findLibraryDetail(String libraryId) {
-        LibraryEntity library =  findLibraryByIdComponent.findLibraryById(libraryId);
+        LibraryEntity library =  libraryMongoRepository.findById(libraryId)
+                .orElseThrow(() -> LibraryNotFoundException.EXCEPTION);
 
         return new LibraryDetailResponse(library, pdfUseCase.getPdfFileUrl(library.getFilePath()));
     }
