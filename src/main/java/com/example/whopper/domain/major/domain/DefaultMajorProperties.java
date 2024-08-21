2f25d4efb28ff32957f34b481319009d20d40e26
101 <tmddnjs001133@dsm.hs.kr>
2024-08-21 15:57:35 +0900

    hotfix: majorId 제거
    작동을 아예 안 하는 문제가 생겨 실제로 사용하지 않는 필드를 삭제하였습니다

--- src/main/java/com/example/whopper/domain/major/domain/DefaultMajorProperties.java
+++ src/main/java/com/example/whopper/domain/major/domain/DefaultMajorProperties.java
@@ -4,7 +4,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "default.major")
 public record DefaultMajorProperties(
-        String majorId,
         String name
 ) {
 }
