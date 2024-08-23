2a27de0e25c29ea52695836f12e6441ee24b3be3
101 <tmddnjs001133@dsm.hs.kr>
2024-08-23 16:17:01 +0900

    update: file 업로드 로직 재작성

--- src/main/java/com/example/whopper/application/file/impl/PdfService.java
+++ src/main/java/com/example/whopper/application/file/impl/PdfService.java
@@ -2,37 +2,24 @@ package com.example.whopper.application.file.impl;
 
 import com.example.whopper.application.file.usecase.PdfUseCase;
 import com.example.whopper.common.exception.file.ExtensionNotMatchException;
-import com.example.whopper.common.exception.file.PdfUploadFailedException;
-import com.example.whopper.infrastructure.aws.s3.AwsS3FileType;
 import com.example.whopper.infrastructure.aws.s3.AwsS3Properties;
+import io.awspring.cloud.s3.S3Operations;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import software.amazon.awssdk.core.async.AsyncRequestBody;
-import software.amazon.awssdk.services.s3.model.GetObjectRequest;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.presigner.S3Presigner;
-import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
-import software.amazon.awssdk.transfer.s3.S3TransferManager;
-import software.amazon.awssdk.transfer.s3.model.UploadRequest;
 
-import java.io.InputStream;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.UUID;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @Service
 @RequiredArgsConstructor
 public class PdfService implements PdfUseCase {
 
     private final AwsS3Properties awsS3Properties;
-    private final S3TransferManager s3TransferManager;
-    private final S3Presigner s3Presigner;
-
-    private final ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor();
+    private final S3Operations s3Operations;
+    private final FileUploadService fileUploadService;
 
     @Override
     public String savePdf(MultipartFile multipartFile) {
@@ -45,47 +32,14 @@ public class PdfService implements PdfUseCase {
         String date = LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE);
         String key = folder + date + "-" + UUID.randomUUID() + ".pdf";
 
-        uploadFile(multipartFile, key);
+        fileUploadService.uploadFile(multipartFile, key);
 
         return key;
     }
 
     @Override
     public String getPdfFileUrl(String filePath) {
-        GetObjectPresignRequest getObjectPresignRequest = GetObjectPresignRequest.builder()
-                .signatureDuration(Duration.ofHours(4))
-                .getObjectRequest(GetObjectRequest.builder()
-                        .bucket(awsS3Properties.bucket())
-                        .key(filePath)
-                        .build())
-                .build(); 
-
-        return s3Presigner.presignGetObject(getObjectPresignRequest).url().toString();
-    }
-
-    private void uploadFile(MultipartFile multipartFile, String key) {
-        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-                .bucket(awsS3Properties.bucket())
-                .key(key)
-                .acl(AwsS3FileType.PDF.getCannedAcl())
-                .build();
-
-        // MultipartFile의 InputStream을 사용하여 파일을 S3에 업로드
-        try (InputStream inputStream = multipartFile.getInputStream()) {
-            // S3 업로드 수행
-            var uploadFuture = s3TransferManager.upload(UploadRequest.builder()
-                    .putObjectRequest(putObjectRequest)
-                    .requestBody(AsyncRequestBody.fromInputStream(inputStream, multipartFile.getSize(), executorService)) // 수정된 부분
-                    .build()).completionFuture();
-
-            uploadFuture.whenComplete((result, exception) -> {
-                if (exception != null) {
-                    throw PdfUploadFailedException.EXCEPTION;
-                }
-            });
-        } catch (Exception e) {
-            throw PdfUploadFailedException.EXCEPTION;
-        }
+        return s3Operations.createSignedGetURL(awsS3Properties.bucket(), filePath, Duration.ofHours(4)).toString();
     }
 
     private String getExtension(String filename) {
