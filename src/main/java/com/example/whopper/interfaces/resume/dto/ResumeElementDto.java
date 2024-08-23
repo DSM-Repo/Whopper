50843c050a369a202307b9f71887b17ebf70e904
101 <tmddnjs001133@dsm.hs.kr>
2024-08-23 22:50:53 +0900

    add: ResumeElementDto 추가

---
+++ src/main/java/com/example/whopper/interfaces/resume/dto/ResumeElementDto.java
@@ -0,0 +1,89 @@
+package com.example.whopper.interfaces.resume.dto;
+
+import com.example.whopper.domain.resume.ResumeModel;
+
+import java.util.List;
+import java.util.Set;
+
+public class ResumeElementDto {
+
+    public enum Status {
+        ONGOING, DELETED, SUBMITTED, RELEASED
+    }
+
+    public record Writer(
+            String name,
+            SchoolInfo schoolInfo,
+            Major major,
+            String email,
+            Set<String> skillSet,
+            String url
+    ) {
+        public record SchoolInfo(
+                Integer grade,
+                Integer classNumber,
+                String schoolNumber,
+                Integer generation
+        ) {}
+
+        public record Major(
+                String majorId,
+                String majorName
+        ) {}
+    }
+
+    public record Introduce(
+            String heading,
+            String introduce
+    ) {}
+
+    public record Project(
+            String name,
+            Logo projectLogo,
+            Type type,
+            Date date,
+            Set<String> skillSet,
+            List<Section> sections,
+            String url
+    ) {
+
+        public enum Type {
+            TEAM, PERSONAL
+        }
+
+        public record Section(
+                String title,
+                String description
+        ) {
+        }
+
+        public record Logo(
+                String imagePath,
+                String originalName
+        ) {
+        }
+    }
+
+    public record Achievement(
+            String name,
+            String institution,
+            String date,
+            Type type
+    ) {
+        public enum Type {
+            AWARD, CERTIFICATE
+        }
+    }
+
+    public record Activity(
+            String name,
+            Date date,
+            boolean isPeriod,
+            String description
+    ) {}
+
+    public record Date(
+            String startDate,
+            String endDate
+    ) {}
+}
