a47dc5f514352f288700d4e0d4ac89f23fec026f
101 <tmddnjs001133@dsm.hs.kr>
2024-08-24 20:37:31 +0900

    hotfix: 현재 선생님 정보 가져오기 api 추가

---
+++ src/main/java/com/example/whopper/application/teacher/usecase/GetCurrentTeacherInfoUseCase.java
@@ -0,0 +1,7 @@
+package com.example.whopper.application.teacher.usecase;
+
+import com.example.whopper.interfaces.teacher.dto.GetCurrentTeacherInfoResponse;
+
+public interface GetCurrentTeacherInfoUseCase {
+    GetCurrentTeacherInfoResponse get();
+}
