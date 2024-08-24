a871ae6766ef635f52f7229cd4e50f1e79403605
101 <tmddnjs001133@dsm.hs.kr>
2024-08-24 23:29:21 +0900

    refactor:

--- src/main/java/com/example/whopper/domain/resume/ResumeEntityMapper.java
+++ src/main/java/com/example/whopper/domain/resume/ResumeEntityMapper.java
@@ -18,39 +18,4 @@ public interface ResumeEntityMapper {
         return model.map(this::toEntity);
     }
 
-
-//    public static Optional<ResumeEntity> toOptionalEntity(Optional<ResumeModel> model) {
-//        return model.map(ResumeEntityMapper::toEntity);
-//    }
-//
-//    public static Optional<ResumeModel> toOptionalModel(Optional<ResumeEntity> entity) {
-//        return entity.map(ResumeEntityMapper::toModel);
-//    }
-//
-//    public static ResumeEntity toEntity(ResumeModel model) {
-//        return ResumeEntity.builder()
-//                .id(model.id())
-//                .year(model.year())
-//                .status(ResumeElementMapper.toStatusEntity(model.status()))
-//                .writer(ResumeElementMapper.toWriterEntity(model.writer()))
-//                .introduce(ResumeElementMapper.toIntroduceEntity(model.introduce()))
-//                .projectList(ResumeElementMapper.toProjectEntityList(model.projectList()))
-//                .achievementList(ResumeElementMapper.toAchievementEntityList(model.achievementList()))
-//                .activityList(ResumeElementMapper.toActivityEntityList(model.activityList()))
-//                .build();
-//    }
-//
-//    public static ResumeModel toModel(ResumeEntity entity) {
-//        return ResumeModel.builder()
-//                .id(entity.getId())
-//                .year(entity.getYear())
-//                .status(ResumeElementMapper.toStatusDto(entity.getStatus()))
-//                .writer(ResumeElementMapper.toWriterDto(entity.getWriter()))
-//                .introduce(ResumeElementMapper.toIntroduceDto(entity.getIntroduce()))
-//                .projectList(ResumeElementMapper.toProjectDtoList(entity.getProjectList()))
-//                .achievementList(ResumeElementMapper.toAchievementDtoList(entity.getAchievementList()))
-//                .activityList(ResumeElementMapper.toActivityDtoList(entity.getActivityList()))
-//                .build();
-//    }
-
 }
