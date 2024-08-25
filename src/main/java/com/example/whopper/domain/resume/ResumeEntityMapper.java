6559a5c90f0cc99f035c3c307d21ca589b4b1f16
101 <tmddnjs001133@dsm.hs.kr>
2024-08-25 20:55:44 +0900

    refactor: mapper componentModel 수정

--- src/main/java/com/example/whopper/domain/resume/ResumeEntityMapper.java
+++ src/main/java/com/example/whopper/domain/resume/ResumeEntityMapper.java
@@ -1,11 +1,12 @@
 package com.example.whopper.domain.resume;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
 
 import java.util.Optional;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, uses = {ResumeElementMapper.class})
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE, uses = {ResumeElementMapper.class})
 public interface ResumeEntityMapper {
     ResumeModel toModel(ResumeEntity entity);
     ResumeEntity toEntity(ResumeModel model);
