7d01b07b29ff613fee3911376b875be192270a06
101 <tmddnjs001133@dsm.hs.kr>
2024-08-26 15:03:30 +0900

    update: findById 추가

--- src/main/java/com/example/whopper/domain/student/StudentRepository.java
+++ src/main/java/com/example/whopper/domain/student/StudentRepository.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 public interface StudentRepository {
     Optional<StudentModel> findByAccountId(String accountId);
+    Optional<StudentModel> findById(String id);
     boolean existsByAccountId(String accountId);
     boolean existsById(String id);
     StudentModel save(StudentModel model);
