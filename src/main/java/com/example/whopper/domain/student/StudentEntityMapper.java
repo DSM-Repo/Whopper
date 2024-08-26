7c18834abf2cfe2a9295ead8f9705426c58ce450
101 <tmddnjs001133@dsm.hs.kr>
2024-08-26 14:58:28 +0900

    update: Optional Mapper 추가

--- src/main/java/com/example/whopper/domain/student/StudentEntityMapper.java
+++ src/main/java/com/example/whopper/domain/student/StudentEntityMapper.java
@@ -4,8 +4,14 @@ import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
 
+import java.util.Optional;
+
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE, uses = StudentElementMapper.class)
 public interface StudentEntityMapper {
     StudentEntity toEntity(StudentModel model);
     StudentModel toModel(StudentEntity entity);
+
+    default Optional<StudentModel> toOptionalModel(Optional<StudentEntity> entity) {
+        return entity.map(this::toModel);
+    }
 }
