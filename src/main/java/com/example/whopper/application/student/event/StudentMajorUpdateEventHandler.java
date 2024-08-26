73236f0938e2838437a78a8e4121a682f1bb7af2
101 <tmddnjs001133@dsm.hs.kr>
2024-08-26 16:28:49 +0900

    hotfix: logging 추가

--- src/main/java/com/example/whopper/application/student/event/StudentMajorUpdateEventHandler.java
+++ src/main/java/com/example/whopper/application/student/event/StudentMajorUpdateEventHandler.java
@@ -3,11 +3,13 @@ package com.example.whopper.application.student.event;
 import com.example.whopper.common.exception.student.StudentNotFoundException;
 import com.example.whopper.domain.student.StudentRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j(topic = "StudentMajorUpdateEventHandler-")
 @RequiredArgsConstructor
 public class StudentMajorUpdateEventHandler {
     private final StudentRepository studentRepository;
@@ -15,8 +17,13 @@ public class StudentMajorUpdateEventHandler {
     @Async
     @EventListener(StudentMajorUpdateEvent.class)
     public void handle(StudentMajorUpdateEvent event) {
+        log.info("StudentMajorUpdateEvent 실행됌!!");
+
         var student = studentRepository.findById(event.studentId())
-                .orElseThrow(() -> StudentNotFoundException.EXCEPTION);
+                .orElseThrow(() -> {
+                    log.error("StudentMajorUpdateEvent 터짐 학생 못 찾음!!");
+                    return StudentNotFoundException.EXCEPTION;
+                });
         var newStudent = student.updateMajor(event.majorId(), event.majorName());
 
         studentRepository.save(newStudent);
