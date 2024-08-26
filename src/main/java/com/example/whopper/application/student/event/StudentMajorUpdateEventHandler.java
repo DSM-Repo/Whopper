01a52555961f1d0b2e7bc8b08acd3c25f89283ed
101 <tmddnjs001133@dsm.hs.kr>
2024-08-26 15:07:32 +0900

    refactor: Student Model 사용

--- src/main/java/com/example/whopper/application/student/event/StudentMajorUpdateEventHandler.java
+++ src/main/java/com/example/whopper/application/student/event/StudentMajorUpdateEventHandler.java
@@ -1,8 +1,7 @@
 package com.example.whopper.application.student.event;
 
 import com.example.whopper.application.student.component.CurrentStudent;
-import com.example.whopper.domain.student.StudentEntity;
-import com.example.whopper.domain.student.StudentMongoRepository;
+import com.example.whopper.domain.student.StudentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
@@ -11,15 +10,15 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 class StudentMajorUpdateEventHandler {
-    private final StudentMongoRepository studentMongoRepository;
+    private final StudentRepository studentRepository;
     private final CurrentStudent currentStudent;
 
     @Async
     @EventListener(StudentMajorUpdateEvent.class)
     public void handle(StudentMajorUpdateEvent event) {
         var student = currentStudent.getStudent();
-        student.updateMajor(new StudentEntity.Major(event.majorId(), event.majorName()));
+        var newStudent = student.updateMajor(event.majorId(), event.majorName());
 
-        studentMongoRepository.save(student);
+        studentRepository.save(newStudent);
     }
 }
