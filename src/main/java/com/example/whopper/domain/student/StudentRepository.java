37b6aeb32adae0089b891117e7c51d54f9232c2e
101 <tmddnjs001133@dsm.hs.kr>
2024-08-26 14:58:45 +0900

    update: StudentRepository 추상 메서드 작성

--- src/main/java/com/example/whopper/domain/student/StudentRepository.java
+++ src/main/java/com/example/whopper/domain/student/StudentRepository.java
@@ -1,4 +1,10 @@
 package com.example.whopper.domain.student;
 
+import java.util.Optional;
+
 public interface StudentRepository {
+    Optional<StudentModel> findByAccountId(String accountId);
+    boolean existsByAccountId(String accountId);
+    boolean existsById(String id);
+    StudentModel save(StudentModel model);
 }
