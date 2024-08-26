ed2d5e935b5c3fce7a3ee6d73a752edf47582a11
101 <tmddnjs001133@dsm.hs.kr>
2024-08-26 14:12:26 +0900

    add: @async 어노테이션을 효과적으로 사용하기 위한 AsyncConfig 작성
    virtual threads base

---
+++ src/main/java/com/example/whopper/common/async/AsyncConfig.java
@@ -0,0 +1,30 @@
+package com.example.whopper.common.async;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.VirtualThreadTaskExecutor;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Configuration
+@EnableAsync(proxyTargetClass = true)
+public class AsyncConfig implements AsyncConfigurer {
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
+        return new VirtualThreadTaskExecutor("repo-virtual-");
+    }
+
+    @Bean(destroyMethod = "close")
+    public ExecutorService virtualThreadExecutor() {
+        return Executors.newVirtualThreadPerTaskExecutor();
+    }
+
+    @Override
+    public Executor getAsyncExecutor() {
+        return taskExecutor();
+    }
+}
