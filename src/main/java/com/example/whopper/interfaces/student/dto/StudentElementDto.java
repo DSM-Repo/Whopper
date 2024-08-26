0b2731cc7331b54274b302f0162a402dd4c2ab7a
101 <tmddnjs001133@dsm.hs.kr>
2024-08-26 14:39:17 +0900

    add: element dtos 추가

---
+++ src/main/java/com/example/whopper/interfaces/student/dto/StudentElementDto.java
@@ -0,0 +1,17 @@
+package com.example.whopper.interfaces.student.dto;
+
+public class StudentElementDto {
+
+    public record ClassInfo(
+            Integer grade,
+            Integer classNumber,
+            Integer number,
+            String schoolNumber
+    ) {
+    }
+
+    public record Major(
+            String id,
+            String name
+    ) {}
+}
