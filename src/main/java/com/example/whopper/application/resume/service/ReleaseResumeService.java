67e642bcdc21d6fe6aa119770c7843508de3632e
101 <tmddnjs001133@dsm.hs.kr>
2024-08-27 22:03:48 +0900

    fix: feedbackRepository 사용

--- src/main/java/com/example/whopper/application/resume/service/ReleaseResumeService.java
+++ src/main/java/com/example/whopper/application/resume/service/ReleaseResumeService.java
@@ -1,11 +1,11 @@
 package com.example.whopper.application.resume.service;
 
 import com.example.whopper.application.resume.usecase.ReleaseResumeUseCase;
+import com.example.whopper.domain.feedback.FeedbackRepository;
 import com.example.whopper.domain.resume.ResumeModel;
 import com.example.whopper.domain.resume.ResumeRepository;
 import com.example.whopper.common.exception.resume.ResumeIllegalStatusException;
 import com.example.whopper.common.exception.resume.ResumeNotFoundException;
-import com.example.whopper.domain.feedback.FeedbackMongoRepository;
 import com.example.whopper.interfaces.resume.dto.ResumeElementDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,7 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 class ReleaseResumeService implements ReleaseResumeUseCase {
     private final ResumeRepository resumeRepository;
-    private final FeedbackMongoRepository feedbackMongoRepository;
+    private final FeedbackRepository feedbackRepository;
 
     @Override
     public void release(String resumeId) {
@@ -36,7 +36,7 @@ class ReleaseResumeService implements ReleaseResumeUseCase {
     }
 
     private void deleteFeedback(String resumeId) {
-        feedbackMongoRepository.deleteAllByResumeId(resumeId);
+        feedbackRepository.deleteAllByResumeId(resumeId);
     }
 
     private ResumeModel findById(String resumeId) {
