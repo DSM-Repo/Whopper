41db7313b977456194b451dee41e70e0694f5d62
dkflfkd53 <b57062562@gmail.com>
2024-08-28 00:57:36 +0900

    refactor :: 구조 수정 및 줄바꿈 추가

--- src/main/java/com/example/whopper/interfaces/resume/dto/request/UpdateWriterInfoRequest.java
+++ src/main/java/com/example/whopper/interfaces/resume/dto/request/UpdateWriterInfoRequest.java
@@ -2,5 +2,9 @@ package com.example.whopper.interfaces.resume.dto.request;
 
 import java.util.Set;
 
-public record UpdateWriterInfoRequest(String email, Set<String> skillSet, String url, String majorId) {
-}
+public record UpdateWriterInfoRequest(
+        String email,
+        Set<String> skillSet,
+        String url,
+        String majorId
+) {}
