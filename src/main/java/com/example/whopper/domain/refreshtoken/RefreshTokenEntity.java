ae6d8bf06ca0125a8b9eeba2b149896ad72c32bb
dkflfkd53 <b57062562@gmail.com>
2024-08-27 20:22:44 +0900

    refactor :: 코드 스타일 통일

--- src/main/java/com/example/whopper/domain/refreshtoken/RefreshTokenEntity.java
+++ src/main/java/com/example/whopper/domain/refreshtoken/RefreshTokenEntity.java
@@ -4,7 +4,6 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
@@ -14,7 +13,6 @@ import org.springframework.data.redis.core.index.Indexed;
 @Getter
 @Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class RefreshTokenEntity {
     @Id
     private String id;
@@ -30,4 +28,6 @@ public class RefreshTokenEntity {
         STUDENT,
         TEACHER
     }
+
+    protected RefreshTokenEntity() {}
 }
