9661d98788627bd805342c5dac12cb2786acd42a
dkflfkd53 <b57062562@gmail.com>
2024-08-28 00:45:06 +0900

    delete :: 사용하지 않는 class 삭제

--- src/main/java/com/example/whopper/application/library/usecase/FindLibraryIndexUseCase.java
+++
@@ -1,7 +0,0 @@
-package com.example.whopper.application.library.usecase;
-
-import com.example.whopper.interfaces.library.dto.response.LibraryIndexResponse;
-
-public interface FindLibraryIndexUseCase {
-    LibraryIndexResponse findLibraryIndex(String libraryId);
-}
