4671fe4c0392d822c317d18e94bad69b74eeede9
101 <tmddnjs001133@dsm.hs.kr>
2024-08-27 21:53:33 +0900

    add: TeacherEntityMapper 생성

---
+++ src/main/java/com/example/whopper/domain/teacher/TeacherEntityMapper.java
@@ -0,0 +1,18 @@
+package com.example.whopper.domain.teacher;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.Optional;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface TeacherEntityMapper {
+
+    TeacherEntity toEntity(TeacherModel model);
+    TeacherModel toEntity(TeacherEntity model);
+
+    default Optional<TeacherModel> toOptionalModel(Optional<TeacherEntity> optionalTeacherEntity) {
+        return optionalTeacherEntity.map(this::toEntity);
+    }
+}
