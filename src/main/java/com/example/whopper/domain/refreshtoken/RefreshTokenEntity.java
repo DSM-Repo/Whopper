b3572df6d968d7fb979d929a4a478aa152c06c4f
dkflfkd53 <b57062562@gmail.com>
2024-08-27 17:26:43 +0900

    refactor :: domain 계층 분리

--- src/main/java/com/example/whopper/domain/refreshtoken/RefreshTokenEntity.java
+++ src/main/java/com/example/whopper/domain/refreshtoken/RefreshTokenEntity.java
@@ -1,6 +1,5 @@
 package com.example.whopper.domain.refreshtoken;
 
-import com.example.whopper.domain.refreshtoken.type.UserRole;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,16 +15,19 @@ import org.springframework.data.redis.core.index.Indexed;
 @Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class RefreshTokenEntity {
-
+class RefreshTokenEntity {
     @Id
     private String id;
-
-    private UserRole userRole;
+    private UserRole role;
 
     @Indexed
     private String token;
 
     @TimeToLive
     private Long timeToLive;
+
+    enum UserRole {
+        STUDENT,
+        TEACHER
+    }
 }
