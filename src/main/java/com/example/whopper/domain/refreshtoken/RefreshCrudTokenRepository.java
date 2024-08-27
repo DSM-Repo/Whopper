8234427b11b9e67a69a025d39c41c45b3b3de76a
dkflfkd53 <b57062562@gmail.com>
2024-08-27 20:00:13 +0900

    refactor :: refreshTokenRepository DIP 적용

---
+++ src/main/java/com/example/whopper/domain/refreshtoken/RefreshCrudTokenRepository.java
@@ -0,0 +1,9 @@
+package com.example.whopper.domain.refreshtoken;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface RefreshCrudTokenRepository extends CrudRepository<RefreshTokenEntity, String> {
+    Optional<RefreshTokenEntity> findByToken(String token);
+}
--- src/main/java/com/example/whopper/domain/refreshtoken/RefreshTokenRepository.java
+++ src/main/java/com/example/whopper/domain/refreshtoken/RefreshTokenRepository.java
@@ -1,9 +1,8 @@
 package com.example.whopper.domain.refreshtoken;
 
-import org.springframework.data.repository.CrudRepository;
-
 import java.util.Optional;
 
-public interface RefreshTokenRepository extends CrudRepository<RefreshTokenEntity, String> {
-    Optional<RefreshTokenEntity> findByToken(String token);
+public interface RefreshTokenRepository {
+    RefreshTokenModel save(RefreshTokenModel refreshTokenModel);
+    Optional<RefreshTokenModel> findByToken(String token);
 }
