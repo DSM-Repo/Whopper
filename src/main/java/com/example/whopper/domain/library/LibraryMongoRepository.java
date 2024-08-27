e090f30a61c17e9aa660456cec857b9881ede909
101 <tmddnjs001133@dsm.hs.kr>
2024-08-27 16:05:39 +0900

    refactor: 접근 제어자, 의도 명확하게 메서드 수정

--- src/main/java/com/example/whopper/domain/library/LibraryMongoRepository.java
+++ src/main/java/com/example/whopper/domain/library/LibraryMongoRepository.java
@@ -1,14 +1,13 @@
 package com.example.whopper.domain.library;
 
-import com.example.whopper.interfaces.library.dto.LibraryElementDto;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
 import java.util.stream.Stream;
 
-public interface LibraryMongoRepository extends MongoRepository<LibraryEntity, String> {
-    Optional<LibraryEntity> findFirstByAccessRightNotAndYear(LibraryElementDto.AccessRight accessRight, int year);
-    Optional<LibraryEntity> findFirstByAccessRightNot(LibraryElementDto.AccessRight accessRight);
+interface LibraryMongoRepository extends MongoRepository<LibraryEntity, String> {
+    Optional<LibraryEntity> findFirstByAccessRightNotAndYear(LibraryEntity.AccessRight accessRight, Integer year);
+    Optional<LibraryEntity> findFirstByAccessRightNot(LibraryEntity.AccessRight accessRight);
     Optional<LibraryEntity> findFirstByYear(int year);
     Stream<LibraryEntity> findTop3ByOrderByCreateAtDesc();
     Stream<LibraryEntity> findAllByYear(int year);
--- src/main/java/com/example/whopper/domain/library/LibraryRepositoryImpl.java
+++ src/main/java/com/example/whopper/domain/library/LibraryRepositoryImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.stream.Stream;
+
 @Repository
 @RequiredArgsConstructor
 class LibraryRepositoryImpl implements LibraryRepository {
@@ -26,35 +27,41 @@ class LibraryRepositoryImpl implements LibraryRepository {
 
     @Override
     public Optional<LibraryModel> findFirstByAccessRightNotAndYear(LibraryElementDto.AccessRight accessRight, int year) {
-        return libraryEntityMapper.toOptionalModel(libraryMongoRepository.findFirstByAccessRightNotAndYear(accessRight, year));
+        return libraryEntityMapper.toOptionalModel(libraryMongoRepository.findFirstByAccessRightNotAndYear(LibraryEntity.AccessRight.valueOf(accessRight.name()), year));
     }
 
     @Override
     public Optional<LibraryModel> findFirstByAccessRightNot(LibraryElementDto.AccessRight accessRight) {
-        return libraryEntityMapper.toOptionalModel(libraryMongoRepository.findFirstByAccessRightNot(accessRight));
+        return libraryEntityMapper.toOptionalModel(libraryMongoRepository.findFirstByAccessRightNot(LibraryEntity.AccessRight.valueOf(accessRight.name())));
     }
 
     @Override
     public Optional<LibraryModel> findFirstByYear(int year) {
-        return libraryMongoRepository.findFirstByYear(year)
-                .map(libraryEntityMapper::toModel);
+        return libraryEntityMapper.toOptionalModel(libraryMongoRepository.findFirstByYear(year));
     }
 
     @Override
     public Stream<LibraryModel> findAll() {
-        return libraryMongoRepository.findAll().stream()
-                .map(libraryEntityMapper::toModel);
+        final var result = libraryMongoRepository.findAll().stream();
+
+        return toStreamLibraryModel(result);
     }
 
     @Override
     public Stream<LibraryModel> findTop3ByOrderByCreateAtDesc() {
-        return libraryMongoRepository.findTop3ByOrderByCreateAtDesc()
-                .map(libraryEntityMapper::toModel);
+        final var result = libraryMongoRepository.findTop3ByOrderByCreateAtDesc();
+
+        return toStreamLibraryModel(result);
     }
 
     @Override
     public Stream<LibraryModel> findAllByYear(int year) {
-        return libraryMongoRepository.findAllByYear(year)
-                .map(libraryEntityMapper::toModel);
+        final var result = libraryMongoRepository.findAllByYear(year);
+
+        return toStreamLibraryModel(result);
+    }
+
+    private Stream<LibraryModel> toStreamLibraryModel(Stream<LibraryEntity> entityStream) {
+        return entityStream.map(libraryEntityMapper::toModel);
     }
 }
