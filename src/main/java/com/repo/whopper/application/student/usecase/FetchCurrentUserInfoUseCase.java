0b859205c686c01d3dad00aebc39cabf42901194
101 <tmddnjs001133@dsm.hs.kr>
2024-08-29 12:29:04 +0900

    refactor: Get -> Fetch 레포 유비쿼터스 언어 적용

---
+++ src/main/java/com/repo/whopper/application/student/usecase/FetchCurrentUserInfoUseCase.java
@@ -0,0 +1,8 @@
+package com.repo.whopper.application.student.usecase;
+
+import com.repo.whopper.interfaces.student.dto.CurrentUserInfoResponse;
+
+public interface FetchCurrentUserInfoUseCase {
+    CurrentUserInfoResponse fetch();
+
+}
