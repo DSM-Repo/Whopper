55534b9e52bc24f2b57f8f551414e207d95e051a
101 <tmddnjs001133@dsm.hs.kr>
2024-08-29 12:29:21 +0900

    refactor: 레포 유비쿼터스 언어 적용

--- src/main/java/com/repo/whopper/application/student/service/FetchCurrentUserInfoService.java
+++ src/main/java/com/repo/whopper/application/student/service/FetchCurrentUserInfoService.java
@@ -1,21 +1,21 @@
 package com.repo.whopper.application.student.service;
 
-import com.repo.whopper.application.student.usecase.GetCurrentUserInfoUseCase;
-import com.repo.whopper.interfaces.student.dto.GetCurrentUserInfoResponse;
+import com.repo.whopper.application.student.usecase.FetchCurrentUserInfoUseCase;
+import com.repo.whopper.interfaces.student.dto.CurrentUserInfoResponse;
 import com.repo.whopper.application.student.component.CurrentStudent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class GetCurrentUserInfoService implements GetCurrentUserInfoUseCase {
+public class FetchCurrentUserInfoService implements FetchCurrentUserInfoUseCase {
     private final CurrentStudent currentStudent;
 
     @Override
-    public GetCurrentUserInfoResponse get() {
+    public CurrentUserInfoResponse fetch() {
         final var currentStudent = this.currentStudent.getStudent();
 
-        return GetCurrentUserInfoResponse.fromStudentEntity(
+        return CurrentUserInfoResponse.fromStudentModel(
                 currentStudent
         );
     }
