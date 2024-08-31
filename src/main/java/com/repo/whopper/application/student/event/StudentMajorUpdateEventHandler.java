6565b4f8bc696e92da52429c34eeea04f7902158
101 <tmddnjs001133@dsm.hs.kr>
2024-08-31 18:27:23 +0900

    hotfix: Event가 Transaction commt 후 핸들하도록 변경

--- src/main/java/com/repo/whopper/application/student/event/StudentMajorUpdateEventHandler.java
+++ src/main/java/com/repo/whopper/application/student/event/StudentMajorUpdateEventHandler.java
@@ -3,22 +3,20 @@ package com.repo.whopper.application.student.event;
 import com.repo.whopper.common.exception.student.StudentNotFoundException;
 import com.repo.whopper.domain.student.StudentRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
-@Slf4j(topic = "StudentMajorUpdateEventHandler-")
 @RequiredArgsConstructor
 public class StudentMajorUpdateEventHandler {
     private final StudentRepository studentRepository;
 
     @Async
-    @EventListener(StudentMajorUpdateEvent.class)
+    @Transactional
+    @TransactionalEventListener(StudentMajorUpdateEvent.class)
     public void handle(StudentMajorUpdateEvent event) {
-        log.info("StudentMajorUpdateEvent 실행됌!!");
-
         final var student = studentRepository.findById(event.studentId())
                 .orElseThrow(() -> StudentNotFoundException.EXCEPTION);
         final var newStudent = student.updateMajor(event.majorId(), event.majorName());
