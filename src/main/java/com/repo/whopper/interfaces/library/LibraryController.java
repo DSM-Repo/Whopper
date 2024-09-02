a6c8072d07faeceb09ddf9ecab64b8a4d9c204c2
dkflfkd53 <b57062562@gmail.com>
2024-09-02 14:18:35 +0900

    refactor :: library 표현 계층 SRP 적용

--- src/main/java/com/repo/whopper/interfaces/library/LibraryController.java
+++ src/main/java/com/repo/whopper/interfaces/library/LibraryController.java
@@ -1,37 +1,21 @@
 package com.repo.whopper.interfaces.library;
 
-import com.repo.whopper.application.file.usecase.PdfUseCase;
 import com.repo.whopper.application.library.usecase.ChangeLibraryAccessRightUseCase;
-import com.repo.whopper.application.library.usecase.CreateLibraryUseCase;
 import com.repo.whopper.application.library.usecase.FindLibraryUseCase;
 import com.repo.whopper.interfaces.library.dto.LibraryElementDto;
-import com.repo.whopper.interfaces.library.dto.request.ResumeIndexRequest;
 import com.repo.whopper.interfaces.library.dto.response.LibraryDetailResponse;
 import com.repo.whopper.interfaces.library.dto.response.LibraryResponse;
 import com.repo.whopper.common.annotation.OnlyTeacher;
 import com.repo.whopper.common.http.dto.DataResponseInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/library")
 public class LibraryController {
-    private final CreateLibraryUseCase createLibraryUseCase;
-    private final PdfUseCase pdfUseCase;
     private final ChangeLibraryAccessRightUseCase changeLibraryAccessRightUseCase;
     private final FindLibraryUseCase findLibraryUseCase;
-
-    @PostMapping
-    public void saveLibraryResume(
-            @RequestParam(name = "grade") Integer grade,
-            @RequestPart(name = "pdf") MultipartFile pdfPart,
-            @RequestPart(name = "index") ResumeIndexRequest indexPart
-    ) {
-        String filePath = pdfUseCase.savePdf(pdfPart);
-        createLibraryUseCase.createLibrary(grade, filePath, DataResponseInfo.of(indexPart.index()));
-    }
 
     @GetMapping("/{libraryId}")
     public LibraryDetailResponse getLibraryDetailResponse(@PathVariable String libraryId) {
