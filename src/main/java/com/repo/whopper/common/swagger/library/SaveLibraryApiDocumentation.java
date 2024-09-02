9af90c8e0dd02a3ee4a39561ee01180e30782f4e
dkflfkd53 <b57062562@gmail.com>
2024-09-02 14:18:09 +0900

    add: library api 문서 설정 추가

---
+++ src/main/java/com/repo/whopper/common/swagger/library/SaveLibraryApiDocumentation.java
@@ -0,0 +1,44 @@
+package com.repo.whopper.common.swagger.library;
+
+import com.repo.whopper.common.error.ErrorResponse;
+import com.repo.whopper.interfaces.library.dto.request.ResumeIndexRequest;
+import com.repo.whopper.interfaces.student.dto.CurrentStudentInfoResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.MediaType;
+import org.springframework.web.multipart.MultipartFile;
+
+@Tag(name = "library", description = "도서관 문서 저장 API")
+public interface SaveLibraryApiDocumentation {
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "OK, 성공!",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(
+                                    implementation = CurrentStudentInfoResponse.class
+                            )
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "학생을 찾지 못했습니다.",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(
+                                    implementation = ErrorResponse.class
+                            )
+                    )
+            )
+    })
+    @Operation(
+            summary = "현재 로그인된 학생 정보 조회 API",
+            description = "클라이언트 요청 authorization 헤더의 토큰을 사용하여 id를 추출하고, 학생을 검색합니다."
+    )
+    void saveLibrary(Integer grade, MultipartFile pdfPart, ResumeIndexRequest indexPart);
+}
