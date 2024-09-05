8ebb022f646a439b1271f625de3c0566981e00bb
101 <tmddnjs001133@dsm.hs.kr>
2024-09-05 09:34:53 +0900

    add: revise controller

---
+++ src/main/java/com/repo/whopper/interfaces/resume/ResumeReviseController.java
@@ -0,0 +1,23 @@
+package com.repo.whopper.interfaces.resume
+
+import com.repo.whopper.application.resume.usecase.ResumeReviseUseCase;
+import com.repo.whopper.common.annotation.OnlyStudent;
+import com.repo.whopper.interfaces.resume.dto.ResumeElementDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/resume")
+class ResumeReviseController {
+    private final ResumeReviseUseCase resumeReviseUseCase;
+
+    @OnlyStudent
+    @PatchMapping
+    void revise(@RequestBody ResumeElementDto.ReviseRequest request) {
+        resumeReviseUseCase.revise(request);
+    }
+}
