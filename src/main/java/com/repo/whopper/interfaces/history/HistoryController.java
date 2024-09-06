17ee9ff3ab9e3c22dd81efb5f7486a5f800ae919
101 <tmddnjs001133@dsm.hs.kr>
2024-09-06 19:06:24 +0900

    hotfix: list 반환 타입 json으로 반환하도록 변경

--- src/main/java/com/repo/whopper/interfaces/history/HistoryController.java
+++ src/main/java/com/repo/whopper/interfaces/history/HistoryController.java
@@ -4,6 +4,7 @@ import com.repo.whopper.application.history.usecase.CreateHistoryUseCase;
 import com.repo.whopper.application.history.usecase.DeleteHistoryUseCase;
 import com.repo.whopper.application.history.usecase.ViewHistoryUseCase;
 import com.repo.whopper.common.annotation.OnlyTeacher;
+import com.repo.whopper.common.http.response.DataResponseInfo;
 import com.repo.whopper.interfaces.history.dto.HistoryResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -20,8 +21,10 @@ class HistoryController {
 
     @OnlyTeacher
     @GetMapping
-    List<HistoryResponse> viewAll() {
-        return viewHistoryUseCase.viewAll();
+    DataResponseInfo<HistoryResponse> viewAll() {
+        final var result = viewHistoryUseCase.viewAll();
+        
+        return DataResponseInfo.of(result);
     }
 
     @OnlyTeacher
