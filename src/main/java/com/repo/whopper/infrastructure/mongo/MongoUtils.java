6031baf21dd40aebfd91b23135e61e7e29c9e694
101 <tmddnjs001133@dsm.hs.kr>
2024-09-14 17:21:30 +0900

    hotfix:

--- src/main/java/com/repo/whopper/infrastructure/mongo/MongoUtils.java
+++ src/main/java/com/repo/whopper/infrastructure/mongo/MongoUtils.java
@@ -1,9 +1,11 @@
 package com.repo.whopper.infrastructure.mongo;
 
+import org.springframework.data.mongodb.core.BulkOperations;
 import org.springframework.data.mongodb.core.query.Query;
 
 import java.util.stream.Stream;
 
 public interface MongoUtils {
     <T> Stream<T> find(Query query, Class<T> targetClass);
+    <T> BulkOperations bulkOps(Class<T> targetClass);
 }
--- src/main/java/com/repo/whopper/infrastructure/mongo/MongoUtilsImpl.java
+++ src/main/java/com/repo/whopper/infrastructure/mongo/MongoUtilsImpl.java
@@ -1,6 +1,7 @@
 package com.repo.whopper.infrastructure.mongo;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.BulkOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Component;
@@ -12,7 +13,13 @@ import java.util.stream.Stream;
 public class MongoUtilsImpl implements MongoUtils {
     private final MongoTemplate mongoTemplate;
 
+    @Override
     public <T> Stream<T> find(Query query, Class<T> targetClass) {
         return mongoTemplate.find(query, targetClass).stream();
+    }
+
+    @Override
+    public <T> BulkOperations bulkOps(Class<T> targetClass) {
+        return mongoTemplate.bulkOps(BulkOperations.BulkMode.UNORDERED, targetClass);
     }
 }
