e2c77a1918d192e4776f858ad3774ede8d690fe8
dkflfkd53 <b57062562@gmail.com>
2024-09-23 19:15:38 +0900

    add :: 공고 조회 api application 계층 추가

--- src/main/java/com/repo/whopper/application/notice/impl/FetchNoticeService.java
+++ src/main/java/com/repo/whopper/application/notice/impl/FetchNoticeService.java
@@ -1,7 +1,9 @@
 package com.repo.whopper.application.notice.impl;
 
 import com.repo.whopper.application.notice.usecase.FetchNoticeUseCase;
-import com.repo.whopper.common.http.response.DataResponseInfo;
+import com.repo.whopper.common.exception.notice.NoticeNotFoundException;
+import com.repo.whopper.common.http.dto.DataResponseInfo;
+import com.repo.whopper.domain.notice.NoticeModel;
 import com.repo.whopper.domain.notice.NoticeRepository;
 import com.repo.whopper.interfaces.notice.dto.response.NoticeDetailResponse;
 import com.repo.whopper.interfaces.notice.dto.response.NoticeResponse;
@@ -16,13 +18,23 @@ public class FetchNoticeService implements FetchNoticeUseCase {
 
     @Override
     @Transactional(readOnly = true)
-    public DataResponseInfo<NoticeResponse> fetchNotice() {
-        return noticeRepository.
+    public NoticeDetailResponse fetchNoticeDetailResponse(String noticeId){
+        NoticeModel notice = noticeRepository.findById(noticeId)
+                .orElseThrow(() -> NoticeNotFoundException.EXCEPTION);
+
+        var newNotice = notice.checkNotice();
+        noticeRepository.save(newNotice);
+
+        return new NoticeDetailResponse(notice);
     }
 
     @Override
     @Transactional(readOnly = true)
-    public NoticeDetailResponse fetchNoticeDetailResponse(String noticeId){
+    public DataResponseInfo<NoticeResponse> fetchNotice() {
+        var notice = noticeRepository.findAll();
 
+        return DataResponseInfo.of(notice
+                .map(NoticeResponse::fromModel)
+                .toList());
     }
 }
--- src/main/java/com/repo/whopper/application/notice/usecase/FetchNoticeUseCase.java
+++ src/main/java/com/repo/whopper/application/notice/usecase/FetchNoticeUseCase.java
@@ -1,6 +1,6 @@
 package com.repo.whopper.application.notice.usecase;
 
-import com.repo.whopper.common.http.response.DataResponseInfo;
+import com.repo.whopper.common.http.dto.DataResponseInfo;
 import com.repo.whopper.interfaces.notice.dto.response.NoticeDetailResponse;
 import com.repo.whopper.interfaces.notice.dto.response.NoticeResponse;
 
