8b702d8696ecb8d9535cce9a20648af233203664
dkflfkd53 <b57062562@gmail.com>
2024-09-23 18:48:05 +0900

    add :: 공고 조회 api 컨트롤러 추가

---
+++ src/main/java/com/repo/whopper/interfaces/notice/FetchNoticeController.java
@@ -0,0 +1,28 @@
+package com.repo.whopper.interfaces.notice;
+
+import com.repo.whopper.application.notice.usecase.FetchNoticeUseCase;
+import com.repo.whopper.common.http.dto.DataResponseInfo;
+import com.repo.whopper.interfaces.notice.dto.response.NoticeDetailResponse;
+import com.repo.whopper.interfaces.notice.dto.response.NoticeResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/notice")
+public class FetchNoticeController {
+    private final FetchNoticeUseCase fetchNoticeUseCase;
+
+    @GetMapping
+    public DataResponseInfo<NoticeResponse> fetchNotice() {
+        return fetchNoticeUseCase.fetchNotice();
+    }
+
+    @GetMapping("/{noticeId}")
+    public NoticeDetailResponse fetchNoticeDetailResponse(@PathVariable String noticeId) {
+        return fetchNoticeUseCase.fetchNoticeDetailResponse(noticeId);
+    }
+}
