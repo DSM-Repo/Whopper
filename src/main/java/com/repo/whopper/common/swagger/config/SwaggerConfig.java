130e3fbd83e5ce37c872c9595a206a7f95b7a814
dkflfkd53 <b57062562@gmail.com>
2024-09-24 19:04:55 +0900

    refactor :: api 문서 domain 별로 그룹화

--- src/main/java/com/repo/whopper/common/swagger/config/SwaggerConfig.java
+++ src/main/java/com/repo/whopper/common/swagger/config/SwaggerConfig.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -37,5 +38,62 @@ public class SwaggerConfig {
                 .title("Spring Boot REST API Specifications")
                 .description("Specification")
                 .version("1.0.0");
+    }
+
+    @Bean
+    public GroupedOpenApi getAuthApi() {
+        return createGroupedOpenApi("auth", "/auth/**");
+    }
+
+    @Bean
+    public GroupedOpenApi getFeedbackApi() {
+        return createGroupedOpenApi("feedback", "/feedback/**");
+    }
+
+    @Bean
+    public GroupedOpenApi getFileApi() {
+        return createGroupedOpenApi("file", "/file/**");
+    }
+
+    @Bean
+    public GroupedOpenApi getHistoryApi() {
+        return createGroupedOpenApi("history", "/history/**");
+    }
+
+    @Bean
+    public GroupedOpenApi getLibraryApi() {
+        return createGroupedOpenApi("library", "/library/**");
+    }
+
+    @Bean
+    public GroupedOpenApi getMajorApi() {
+        return createGroupedOpenApi("major", "/major/**");
+    }
+
+    @Bean
+    public GroupedOpenApi getNoticeApi() {
+        return createGroupedOpenApi("notice", "/notice/**");
+    }
+
+    @Bean
+    public GroupedOpenApi getResumeApi() {
+        return createGroupedOpenApi("resume", "/resume/**");
+    }
+
+    @Bean
+    public GroupedOpenApi getStudentApi() {
+        return createGroupedOpenApi("student", "/user/**");
+    }
+
+    @Bean
+    public GroupedOpenApi getTeacherApi() {
+        return createGroupedOpenApi("teacher", "/teacher/**");
+    }
+
+    private GroupedOpenApi createGroupedOpenApi(String groupName, String path) {
+        return GroupedOpenApi.builder()
+                .group(groupName)
+                .pathsToMatch(path)
+                .build();
     }
 }
